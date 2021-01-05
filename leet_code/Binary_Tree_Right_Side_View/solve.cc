08e4117f3f8e68a3de2564167428d869491ca2d4
ldy121 <dy121.lee@gmail.com>
2021-01-05 22:20:12 +0900

    add Binary Tree Right Side View problem

---
+++ leet_code/Binary_Tree_Right_Side_View/solve.cc
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<int> vec;
+        
+        if (root == NULL) {
+            return vec;
+        }
+        
+        for (q.push(root); !q.empty();) {
+            vec.push_back(q.front()->val);
+
+            for (int size = q.size(); size > 0; --size) {
+                TreeNode *node = q.front();
+                q.pop();
+                if (node->right) q.push(node->right);
+                if (node->left) q.push(node->left);
+            }
+        }
+        
+        return vec;
+    } 
+};
